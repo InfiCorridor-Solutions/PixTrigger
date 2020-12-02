d497536e47034f13e602eb41e3885e1c19c668e5
christoaluckal <christo12aluckal@gmail.com>
2020-12-02 12:48:21 +0530

    CPP

---
+++ src/capture.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <gphoto2/gphoto2.h>
+
+#include "defines.h"
+#include <time.h>
+
+static void
+capture_to_file(Camera *camera, GPContext *context, char *fn);
+void capture_image(std::string path, std::string imagename);
